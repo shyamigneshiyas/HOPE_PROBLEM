ec1f9940b4e9f83a75b49078d5a42954ee314284
shyamigneshiyas <shyamigneshiyas2006@gmail.com>
2026-03-24 11:24:09 +0530

    Delete bulbswitcher.java

--- bulbswitcher.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class bulbswitcher {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.print((int)Math.sqrt(n));
-
-    }
-}
